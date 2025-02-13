ba63304221110afd4faf7c72b0539a6982516972
joshfisidi <josh@fisidi.com>
2025-02-13 18:29:50 -0500

    setting up basic routes

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,7 +2,7 @@ import type { Config } from "tailwindcss"
 import animate from "tailwindcss-animate"
 
 const config = {
-  darkMode: ["class"],
+  darkMode: "class",
   content: [
     './src/pages/**/*.{ts,tsx}',
     './src/components/**/*.{ts,tsx}',
