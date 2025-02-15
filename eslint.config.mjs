fd282b2e4f4103a574523749a11484d869d83add
joshfisidi <josh@fisidi.com>
2025-02-15 18:53:38 -0500

    authentication

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,31 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import nextPlugin from '@next/eslint-plugin'
+import tsParser from '@typescript-eslint/parser'
+import globals from 'globals'
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
-
-export default eslintConfig;
+export default [
+  {
+    files: ['**/*.{js,jsx,ts,tsx}'],
+    plugins: {
+      '@next/next': nextPlugin,
+    },
+    languageOptions: {
+      parser: tsParser,
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+        ...globals.es2021,
+      },
+    },
+    settings: {
+      next: {
+        rootDir: '.',
+      },
+    },
+    extends: [
+      'next/core-web-vitals',
+    ],
+    rules: {
+      '@next/next/no-html-link-for-pages': 'error',
+    },
+  },
+]
